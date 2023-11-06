8ecd2fb32d983bf199c9ad18f19275fd1307c3c5
peeech <pj@imagine-nyc.com>
2023-11-06 20:03:44 +0100

    more crazy integration tests WIP

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,12 +3,46 @@ mod utils;
 // use log::{debug, info};
 use rocket::tokio;
 use utils::Test;
+use log::{debug, info};
+
+use crate::utils::{Happ, to_cell, PresentedHappBundle, HappInput, HappAndHost};
 
 #[tokio::test]
 async fn install_components() {
     env_logger::init();
 
-    let test = Test::init().await;
+    let mut test = Test::init().await;
+
+    let hha_app_info = test.install_app(Happ::HHA).await;
+    let hha_cell = to_cell(hha_app_info, "core-app");
+
+    // publish test happ to hha
+    // howto: https://github.com/Holo-Host/holo-hosting-app-rsm/blob/develop/tests/unit-test/provider-init.ts#L52
+    let payload = HappInput::default();
+    let draft_hha_bundle: PresentedHappBundle = test
+        .call_zome(&hha_cell, "hha", "create_draft", payload)
+        .await;
+    let payload = draft_hha_bundle.id;
+    let hha_bundle: PresentedHappBundle = test
+        .call_zome(&hha_cell, "hha", "publish_happ", payload)
+        .await;
+
+    // enable test happ in hha
+    let payload = HappAndHost {
+        happ_id: hha_bundle.id,
+        holoport_id: "my_holoport".to_string(),
+    };
+    let _: () = test
+        .call_zome(&hha_cell, "hha", "enable_happ", payload)
+        .await;
+
+    info!("Hosted happ enabled in hha - OK");
+
+    // I do not have to actually install hosted happ because I am insterested only in service logger records
+
+    // Install SL for hosted happ with host_agent key
+    let sl_app_info = test.install_app(Happ::SL).await;
+    debug!("sl_app_info: {:#?}", &sl_app_info);
 
     // Start API
 
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,21 +1,33 @@
-use std::{env, path::PathBuf, fs::File};
+use std::{env, path::PathBuf, fs::File, sync::Arc, collections::HashMap, fmt};
+use holochain_conductor_api::{CellInfo, ProvisionedCell};
 use hpos_config_core::*;
 use hpos_config_seed_bundle_explorer::unlock;
 use holochain_env_setup::{
     environment::{setup_environment, Environment},
     holochain::{create_log_dir, create_tmp_dir},
-    // storage_helpers::download_file,
+    storage_helpers::download_file,
 };
-use holochain_types::prelude::AgentPubKey;
-use holochain_client::{AdminWebsocket, AppWebsocket};
+use holochain_types::{prelude::{AgentPubKey, ExternIO, holochain_serial, ZomeCallUnsigned, SerializedBytes, Timestamp, UnsafeBytes, AppBundleSource, Nonce256Bits}, dna::{ActionHashB64, AgentPubKeyB64}};
+use holochain_client::{AdminWebsocket, AppWebsocket, ZomeCall, AppInfo, InstallAppPayload};
+use holofuel_types::fuel::Fuel;
 use hpos_api_rust::consts::{ADMIN_PORT, APP_PORT};
 use anyhow::{anyhow, Context, Result};
 use rocket::serde::json::serde_json;
 use ed25519_dalek::Keypair;
+use log::{debug, info, trace};
+use serde::{Serialize, Deserialize};
+use url::Url;
+use std::time::Duration;
+use std::fmt::Debug;
+
+// https://github.com/Holo-Host/holo-nixpkgs/blob/develop/profiles/logical/happ-releases.nix#L9C5-L9C5
+pub const HHA_URL: &str = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/core-app/0_5_13/core-app.0_5_13-skip-proof.happ";
+pub const SL_URL: &str = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/0_4_18/servicelogger.0_4_18.happ";
+
 
 pub struct Test {
     pub hc_env: Environment,
-    //pub agent: HoloHash<Agent>,
+    pub agent: AgentPubKey,
     pub admin_ws: AdminWebsocket,
     pub app_ws: AppWebsocket,
 }
@@ -37,13 +49,10 @@ impl Test {
         // can import a keypar for an agent from hpos-config
         let (agent_string, device_bundle) = from_config(hpos_config_path.into(), PASSWORD.into()).await.unwrap();
 
-        let bytes = base64::decode_config(&agent_string, base64::URL_SAFE_NO_PAD).unwrap();
-
+        let bytes = base64::decode_config(&agent_string[1..], base64::URL_SAFE_NO_PAD).unwrap();
         let agent: AgentPubKey = AgentPubKey::from_raw_39(bytes).unwrap();
 
-        println!("in: {}, out: {}", &agent_string, agent);
-
-        //println!("agent: {}, bundle: {}", agent_string, device_bundle);
+        info!("agent: {}, bundle: {}", agent, device_bundle);
 
         let tmp_dir = create_tmp_dir();
         let log_dir = create_log_dir();
@@ -53,7 +62,7 @@ impl Test {
             .await
             .expect("Error spinning up Holochain environment");
 
-        println!("Started holochain in tmp dir {:?}", &tmp_dir);
+        info!("Started holochain in tmp dir {:?}", &tmp_dir);
 
         let mut admin_ws = AdminWebsocket::connect(format!("ws://localhost:{}", ADMIN_PORT))
             .await
@@ -69,10 +78,86 @@ impl Test {
 
         Self {
             hc_env,
-            // agent,
+            agent,
             admin_ws,
             app_ws,
         }
+    }
+
+    pub async fn install_app(&mut self, happ: Happ) -> AppInfo {
+        let url = match happ {
+            Happ::HHA => HHA_URL,
+            Happ::SL => SL_URL,
+        };
+
+        // Install happ with host_agent key
+        let mut membrane_proofs = HashMap::new();
+        membrane_proofs.insert(
+            happ.to_string(),
+            Arc::new(SerializedBytes::from(UnsafeBytes::from(vec![0]))),
+        );
+        let hha_path =
+            download_file(&Url::parse(url).expect(&format!("failed to parse {}", stringify!(url))))
+                .await
+                .expect("failed to download happ bundle");
+
+        let payload = InstallAppPayload {
+            agent_key: self.agent.clone(),
+            installed_app_id: Some(happ.to_string()),
+            source: AppBundleSource::Path(hha_path),
+            membrane_proofs,
+            network_seed: None,
+            ignore_genesis_failure: false,
+        };
+
+        let app_info = self
+            .admin_ws
+            .install_app(payload)
+            .await
+            .expect("failed to install happ");
+
+        trace!("{} app_info: {:#?}", happ, &app_info);
+
+        // enable happ
+        let _ = self
+            .admin_ws
+            .enable_app(app_info.installed_app_id.clone())
+            .await
+            .expect("failed to enable app");
+
+        debug!("AppInfo for newly installed app {}: {:#?}", happ, app_info);
+
+        app_info
+    }
+
+    pub async fn call_zome<T: Debug + Serialize, R: Debug + for<'de> Deserialize<'de>>(
+        &mut self,
+        hha_cell: &ProvisionedCell,
+        zome_name: &str,
+        fn_name: &str,
+        payload: T,
+    ) -> R {
+        let (nonce, expires_at) = fresh_nonce();
+
+        let zome_call_unsigned = ZomeCallUnsigned {
+            provenance: self.agent.clone(),
+            cell_id: hha_cell.clone().cell_id,
+            zome_name: zome_name.into(),
+            fn_name: fn_name.into(),
+            cap_secret: None,
+            payload: ExternIO::encode(payload).unwrap(),
+            nonce,
+            expires_at,
+        };
+
+        let signed_zome_call =
+            ZomeCall::try_from_unsigned_zome_call(&self.hc_env.keystore, zome_call_unsigned)
+                .await
+                .unwrap();
+
+        let response = self.app_ws.call_zome(signed_zome_call).await.unwrap();
+
+        ExternIO::decode(&response).unwrap()
     }
 }
 
@@ -92,4 +177,111 @@ async fn from_config(config_path: PathBuf, password: String) -> Result<(String, 
         },
         _ => Err(anyhow!("Unsupported version of hpos config"))
     }
+}
+
+/// generates nonce for zome calls
+/// https://github.com/Holo-Host/hpos-service-crates/blob/e1d1baa0c9741a46a29626ce33d9e019c1391db8/crates/hpos_connect_hc/src/utils.rs#L13
+fn fresh_nonce() -> (Nonce256Bits, Timestamp) {
+    let mut bytes = [0; 32];
+    getrandom::getrandom(&mut bytes).unwrap();
+    let nonce = Nonce256Bits::from(bytes);
+    // Rather arbitrary but we expire nonces after 5 mins.
+    let expires: Timestamp = (Timestamp::now() + Duration::from_secs(60 * 5)).unwrap();
+    (nonce, expires)
+}
+
+/// Extract destructively cell from AppInfo
+pub fn to_cell(mut hha_app_info: AppInfo, role_name: &str) -> ProvisionedCell {
+    let mut a = hha_app_info.cell_info.remove(role_name).unwrap();
+    match a.pop() {
+        Some(CellInfo::Provisioned(hha_cell)) => hha_cell,
+        _ => panic!("Couldn't find cell for hha"),
+    }
+}
+
+pub enum Happ {
+    HHA,
+    SL,
+}
+impl fmt::Display for Happ {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Happ::HHA => write!(f, "hha"),
+            Happ::SL => write!(f, "servicelogger"),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes)]
+pub struct PresentedHappBundle {
+    pub id: ActionHashB64,
+    pub provider_pubkey: AgentPubKeyB64,
+    pub is_draft: bool,
+    pub is_paused: bool,
+    pub uid: Option<String>,
+    pub bundle_url: String,
+    pub ui_src_url: Option<String>,
+    pub dnas: Vec<DnaResource>,
+    pub hosted_urls: Vec<String>,
+    pub name: String,
+    pub logo_url: Option<String>,
+    pub description: String,
+    pub categories: Vec<String>,
+    pub jurisdictions: Vec<String>,
+    pub exclude_jurisdictions: bool,
+    pub login_config: LoginConfig,
+    pub special_installed_app_id: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone, Default)]
+pub struct HappInput {
+    pub hosted_urls: Vec<String>,
+    pub bundle_url: String,
+    pub ui_src_url: Option<String>,
+    pub special_installed_app_id: Option<String>,
+    pub name: String,
+    pub logo_url: Option<String>,
+    pub dnas: Vec<DnaResource>,
+    pub description: String,
+    pub categories: Vec<String>,
+    pub jurisdictions: Vec<String>,
+    pub exclude_jurisdictions: bool,
+    pub publisher_pricing_pref: PublisherPricingPref,
+    pub login_config: LoginConfig,
+    pub uid: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone)]
+pub struct PublisherPricingPref {
+    pub cpu: Fuel,
+    pub storage: Fuel,
+    pub bandwidth: Fuel,
+}
+impl Default for PublisherPricingPref {
+    fn default() -> Self {
+        PublisherPricingPref {
+            cpu: Fuel::new(0),
+            storage: Fuel::new(0),
+            bandwidth: Fuel::new(0),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone, Default)]
+pub struct LoginConfig {
+    pub display_publisher_name: bool,
+    pub registration_info_url: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone)]
+pub struct DnaResource {
+    pub hash: String, // hash of the dna, not a stored dht address
+    pub src_url: String,
+    pub nick: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone)]
+pub struct HappAndHost {
+    pub happ_id: ActionHashB64,
+    pub holoport_id: String,
 }
