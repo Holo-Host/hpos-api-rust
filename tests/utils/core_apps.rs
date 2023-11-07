affc0af91f77e496d105734a24000fbb5e103cb9
peeech <pj@imagine-nyc.com>
2023-11-07 15:43:26 +0100

    bump core-app to 0.5.19

--- tests/utils/core_apps.rs
+++ tests/utils/core_apps.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 // https://github.com/Holo-Host/holo-nixpkgs/blob/develop/profiles/logical/happ-releases.nix#L9C5-L9C5
 // TODO: update release of hha to version with disable_happ in it
-pub const HHA_URL: &str = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/core-app/0_5_18/core-app.0_5_18-skip-proof.happ";
+pub const HHA_URL: &str = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/core-app/0_5_19/core-app.0_5_19-skip-proof.happ";
 pub const SL_URL: &str = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/0_4_18/servicelogger.0_4_18.happ";
 
 pub enum Happ {
@@ -13,7 +13,7 @@ pub enum Happ {
 impl fmt::Display for Happ {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Happ::HHA => write!(f, "core-app:0_5_18"),
+            Happ::HHA => write!(f, "core-app:0_5_19"),
             Happ::SL => write!(f, "servicelogger"),
         }
     }
