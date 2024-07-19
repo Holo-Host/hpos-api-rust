289a60c27cf7537428a0bc33e8ff402e280ad2f5
Patrick Winfield <patrick.winfield@holo.host>
2024-07-19 11:55:38 -0600

    wrap core/version in json and fix tests

--- src/routes/apps/core.rs
+++ src/routes/apps/core.rs
@@ -1,14 +1,24 @@
 use rocket::{
     http::Status,
+    serde::json::Json,
     {get, State},
 };
-
+use serde::Serialize;
 use crate::hpos::WsMutex;
+
+#[derive(Serialize)]
+pub struct VersionResponse {
+    version: String,
+}
 
 /// Return an installed_app_id of a core app
 #[get("/core/version")]
-pub async fn version(wsm: &State<WsMutex>) -> Result<String, (Status, String)> {
+pub async fn version(wsm: &State<WsMutex>) -> Result<Json<VersionResponse>, (Status, String)> {
     let ws = wsm.lock().await;
 
-    Ok(ws.core_app_id.clone())
+    let response = VersionResponse {
+        version: ws.core_app_id.clone(),
+    };
+
+    Ok(Json(response))
 }
